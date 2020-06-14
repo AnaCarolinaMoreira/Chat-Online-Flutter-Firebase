013e0f48b4f74f2b9345174d44885cfe775eb632
AnaCarolinaMoreira <anacarolina35980@gmail.com>
2020-06-14 17:07:54 -0300

    first commit

---
+++ chat/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:br/chat_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:async/async.dart';
+
+void main() async{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        iconTheme: IconThemeData(
+          color: Colors.blue,
+        )
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
